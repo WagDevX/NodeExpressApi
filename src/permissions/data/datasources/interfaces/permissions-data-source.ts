8ad097a03a6cf3b9ed5d5935e6b9c9adb1601b1e
Wagner de Araujo <124446545+WagDevX@users.noreply.github.com>
2024-01-13 22:25:58 +0000

    feat & test: permissions repository added

---
+++ src/permissions/data/datasources/interfaces/permissions-data-source.ts
@@ -0,0 +1,7 @@
+import { Permission } from "../../../domain/entities/permission";
+
+export interface PermissionsDataSource {
+    createPermission(permission: Permission): Promise<void>;
+    updatePermission(id: number, permission: Permission): Promise<void>;
+    getPermissions(): Promise<Permission[]>;
+}
---
+++ src/permissions/data/repos/permissions-repository-impl.ts
@@ -0,0 +1,19 @@
+import { Permission } from "../../domain/entities/permission";
+import { PermissionsRepository } from "../../domain/repos/permissions-repository";
+import { PermissionsDataSource } from "../datasources/interfaces/permissions-data-source";
+
+export class PermissionsRepositoryImpl implements PermissionsRepository {
+    constructor(private readonly dataSource: PermissionsDataSource) {}
+    async createPermission(permission: Permission): Promise<boolean> {
+        await this.dataSource.createPermission(permission);
+        return true;
+    }
+    async updatePermission(id: number, permission: Permission): Promise<boolean> {
+        await this.dataSource.updatePermission(id, permission);
+        return true;
+    }
+    async getPermissions(): Promise<Permission[]> {
+        return await this.dataSource.getPermissions();
+    }
+
+}
