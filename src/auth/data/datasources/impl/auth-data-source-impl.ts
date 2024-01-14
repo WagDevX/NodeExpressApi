f46579a3f11ddbdaa5cdbf9f40f295543d93efbd
WagDevX <waguinbk@gmail.com>
2024-01-13 22:51:29 -0300

    fix: auth data source

--- src/auth/data/datasources/impl/auth-data-source-impl.ts
+++ src/auth/data/datasources/impl/auth-data-source-impl.ts
@@ -10,8 +10,7 @@ export class AuthDataSourceImpl implements AuthDataSource {
 
   async loginWithUserNameAndPassword(user: User): Promise<User> {
     const result = await this.db.query(
-      `SELECT * FROM users WHERE username = ? AND password = ?`,
-      [user.username, user.password]
+      `SELECT * FROM users WHERE username = '${user.username}' AND password = '${user.password}'`
     );
     if (result.rows.length === 0) {
       throw new Error("Authentication failed");
@@ -21,7 +20,7 @@ export class AuthDataSourceImpl implements AuthDataSource {
   async registerUser(user: User): Promise<User> {
     const result = await this.db.query(
       `INSERT INTO users (username, password, role) VALUES (${user.username}, ${user.password}, 'user') \n` +
-      `SELECT * FROM users WHERE username = ${user.username} AND password = ${user.password}`
+        `SELECT * FROM users WHERE username = ${user.username} AND password = ${user.password}`
     );
     return result.rows[0];
   }
@@ -36,9 +35,7 @@ export class AuthDataSourceImpl implements AuthDataSource {
     );
   }
   async changeRole(id: number, role: string): Promise<void> {
-    await this.db.query(
-      `UPDATE users SET role = ${role} WHERE id = ${id};`
-    );
+    await this.db.query(`UPDATE users SET role = ${role} WHERE id = ${id};`);
   }
   async changePassword(
     id: number,
@@ -51,9 +48,7 @@ export class AuthDataSourceImpl implements AuthDataSource {
   }
 
   async getUsers(): Promise<User[]> {
-    const result = await this.db.query(
-      `SELECT * FROM users`
-    );
+    const result = await this.db.query(`SELECT * FROM users`);
     return result.rows;
   }
 }
--- src/auth/presentation/router/auth-router.ts
+++ src/auth/presentation/router/auth-router.ts
@@ -8,92 +8,95 @@ import { RegisterUserUseCase } from "../../domain/usecases/interfaces/register-u
 import { ResetPasswordUseCase } from "../../domain/usecases/interfaces/reset-password";
 
 export default function AuthRouter(
-    loginUseCase : LoginWithUserNameAndPasswordUseCase,
-    registerUseCase : RegisterUserUseCase,
-    changeRoleUseCase : ChangeRoleUseCase,
-    changeUserNameUseCase : ChangeUserNameUseCase,
-    changePasswordUseCase : ChangePasswordUseCase,
-    getUsersUseCase : GetUsersUseCase,
-    resetPasswordUseCase : ResetPasswordUseCase
+  loginUseCase: LoginWithUserNameAndPasswordUseCase,
+  registerUseCase: RegisterUserUseCase,
+  changeRoleUseCase: ChangeRoleUseCase,
+  changeUserNameUseCase: ChangeUserNameUseCase,
+  changePasswordUseCase: ChangePasswordUseCase,
+  getUsersUseCase: GetUsersUseCase,
+  resetPasswordUseCase: ResetPasswordUseCase
 ) {
+  const router = express.Router();
 
-    const router = express.Router();
+  router.get("/getall", async (req: any, res: any) => {
+    try {
+      const users = await getUsersUseCase.execute();
+      res.send(users);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({ message: "Error fetching users" });
+    }
+  });
 
-    router.get("/getall", async (req: any, res: any) => {
-        try {
-            const users = await getUsersUseCase.execute();
-            res.send(users);
-        } catch (err) {
-            console.log(err);
-            res.status(500).send({ message: "Error fetching users" });
-        }
-    });
+  router.post("/login", async (req: any, res: any) => {
+    console.log(res.body);
+    try {
+      const user = await loginUseCase.execute(req.body);
+      res.send(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({ message: "Authentication failed" });
+    }
+  });
 
-    router.post("/login", async (req: any, res: any) => {
-        try {
-            const user = await loginUseCase.execute(req.body);
-            res.send(user);
-        } catch (err) {
-            console.log(err);
-            res.status(500).send({ message: "Error logging in" });
-        }
-    });
+  router.post("/register", async (req: any, res: any) => {
+    try {
+      await registerUseCase.execute(req.body);
+      res.statusCode = 201;
+      res.json({ message: "Created" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({ message: "Error registering user" });
+    }
+  });
 
-    router.post("/register", async (req: any, res: any) => {
-        try {
-            await registerUseCase.execute(req.body);
-            res.statusCode = 201;
-            res.json({ message: "Created" });
-        } catch (err) {
-            console.log(err);
-            res.status(500).send({ message: "Error registering user" });
-        }
-    });
+  router.put("/changerole", async (req: any, res: any) => {
+    try {
+      await changeRoleUseCase.execute(req.body.id, req.body.role);
+      res.statusCode = 201;
+      res.json({ message: "Created" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({ message: "Error changing role" });
+    }
+  });
 
-    router.put("/changerole", async (req: any, res: any) => {
-        try {
-            await changeRoleUseCase.execute(req.body.id, req.body.role);
-            res.statusCode = 201;
-            res.json({ message: "Created" });
-        } catch (err) {
-            console.log(err);
-            res.status(500).send({ message: "Error changing role" });
-        }
-    });
+  router.put("/changeusername", async (req: any, res: any) => {
+    try {
+      await changeUserNameUseCase.execute(req.body.id, req.body.username);
+      res.statusCode = 201;
+      res.json({ message: "Created" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({ message: "Error changing username" });
+    }
+  });
 
-    router.put("/changeusername", async (req: any, res: any) => {
-        try {
-            await changeUserNameUseCase.execute(req.body.id, req.body.username);
-            res.statusCode = 201;
-            res.json({ message: "Created" });
-        } catch (err) {
-            console.log(err);
-            res.status(500).send({ message: "Error changing username" });
-        }
-    });
+  router.put("/changepassword", async (req: any, res: any) => {
+    try {
+      await changePasswordUseCase.execute(
+        req.body.id,
+        req.body.password,
+        req.body.newPassword
+      );
+      res.statusCode = 201;
+      res.json({ message: "Created" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({ message: "Error changing password" });
+    }
+  });
 
-    router.put("/changepassword", async (req: any, res: any) => {
-        try {
-            await changePasswordUseCase.execute(req.body.id, req.body.password, req.body.newPassword);
-            res.statusCode = 201;
-            res.json({ message: "Created" });
-        } catch (err) {
-            console.log(err);
-            res.status(500).send({ message: "Error changing password" });
-        }
-    });
+  router.put("/resetpassword", async (req: any, res: any) => {
+    try {
+      await resetPasswordUseCase.execute(req.body.id, req.body.password);
+      res.statusCode = 201;
+      res.json({ message: "Created" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({ message: "Error resetting password" });
+    }
+  });
 
-    router.put("/resetpassword", async (req: any, res: any) => {
-        try {
-            await resetPasswordUseCase.execute(req.body.id, req.body.password);
-            res.statusCode = 201;
-            res.json({ message: "Created" });
-        } catch (err) {
-            console.log(err);
-            res.status(500).send({ message: "Error resetting password" });
-        }
-    });
-
-    return router;
-
+  return router;
 }
